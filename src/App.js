1085656a3aa51feabe6e40fb03f5c050c6838562
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-05 13:19:44 +0530

    AddBtn, AddLog, EditLog & TechList Modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Fragment, useEffect } from 'react';
 
 import SearchBar from './components/layouts/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layouts/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,6 +22,11 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
