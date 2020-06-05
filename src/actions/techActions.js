b9fbae02e88c76facac69ba0523d6206fd9abf31
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-06 03:09:43 +0530

    Delete Technician

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -40,6 +40,21 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+// Delete tech
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE',
+    });
+
+    dispatch({ type: DELETE_TECH, payload: id });
+  } catch (err) {
+    dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
+  }
+};
+
 // Set loading
 export const setLoading = () => {
   return {
