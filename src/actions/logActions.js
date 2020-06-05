298bfbc8d112fe798ce89ee46a3bf9c029e305d7
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-06-05 23:10:46 +0530

    Search Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from './types';
 
 /* export const getLogs = () => {
@@ -84,6 +85,20 @@ export const updateLog = (log) => async (dispatch) => {
     const data = await res.json();
 
     dispatch({ type: UPDATE_LOG, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Search logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/?q=${text}`);
+    const data = await res.json();
+
+    dispatch({ type: SEARCH_LOGS, payload: data });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
